054b45ceebb22760a48ca97a246765e5841414e0
Steve Luzynski <steve@luzynski.net>
2017-12-06 13:13:23 -0600

    did some optimizing on day5p2 because that time was shameful

--- day5p2.py
+++ day5p2.py
@@ -8,22 +8,23 @@ with open('test.txt') as f:
     for line in f:
         lines.append(int(line))
 
-print len(lines)
+length = len(lines)
 
+def calc(length):
+    pc = 0
+    jump = 0
+    count = 0
 
-pc = 0
-jump = 0
-count = 0
+    while pc < length:
+        #print("pc ", pc, " count ", count)
+        count += 1 # increment instruction counter
+        #print lines
+        jump = lines[pc] # how far to jump on this run
+        if jump >= 3:
+            lines[pc] -= 1
+        else:
+            lines[pc] += 1 # increment value at current position
+        pc += jump # add value of current position to pc
+    return count
 
-while pc < len(lines):
-    #print("pc ", pc, " count ", count)
-    count += 1 # increment instruction counter
-    #print lines
-    jump = lines[pc] # how far to jump on this run
-    if jump >= 3:
-        lines[pc] -= 1
-    else:
-        lines[pc] += 1 # increment value at current position
-    pc += jump # add value of current position to pc
-
-print count
+print calc(length)
